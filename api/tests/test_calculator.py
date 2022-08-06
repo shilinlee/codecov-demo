e64f5eaa250d9bf197add84d1e63f13adeb6b336
shilinlee <836160610@qq.com>
2022-08-06 14:17:21 +0800

    fix

--- api/tests/test_calculator.py
+++ api/tests/test_calculator.py
@@ -34,7 +34,7 @@ def test_divide_by_0():
     assert Calculator.divide(2.0, 0) == 'Cannot divide by 0'
 
 
-def test_add():
+def test_add2():
     assert Calculator2.add(1, 2) == 3.0
     assert Calculator2.add(1.0, 2.0) == 3.0
     assert Calculator2.add(0, 2.0) == 2.0
